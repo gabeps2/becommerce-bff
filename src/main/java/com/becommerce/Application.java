e9863caeded2b9deec0ce3137b1f0ff5d8bcdb1b
Gabriel <gabriel.psilva226@gmail.com>
2022-09-14 21:45:28 +0000

    Initial commit

---
+++ src/main/java/com/becommerce/Application.java
@@ -0,0 +1,9 @@
+package com.becommerce;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
